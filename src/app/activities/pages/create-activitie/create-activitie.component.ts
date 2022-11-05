4ebb2acdb6e7997b2141316366ac7f10527a312c
jcarloshg <carlosj12336@gmail.com>
2022-11-04 18:10:17 -0600

    add pages [CreateActivitieComponent]

--- src/app/activities/pages/create-activitie/create-activitie.component.ts
+++ src/app/activities/pages/create-activitie/create-activitie.component.ts
@@ -1,5 +1,29 @@
 import { Component, OnInit } from '@angular/core';
 
+// name             |
+// activitieLevel   |   ActivityLevel
+// typeActivity     |   ActivityType
+// question
+// questionBody
+// answers
+// topicID
+
+// enum ActivityLevel {
+//   A1
+//   A2
+//   B1
+//   B2
+//   C1
+//   C2
+// }
+
+// enum ActivityType {
+//   WRITING
+//   READING
+//   TALKING
+//   LISTENING
+// }
+
 @Component({
   selector: 'app-create-activitie',
   templateUrl: './create-activitie.component.html',
