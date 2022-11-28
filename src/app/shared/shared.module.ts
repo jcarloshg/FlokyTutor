bb737bda05b0816ba18590ec3e5e78af05f7a972
jcarloshg <carlosj12336@gmail.com>
2022-11-27 18:04:55 -0600

    modify padding from navigation into [home]

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,25 +2,24 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 // mine
-import { CollegeEnrollmentInputComponent } from './inputs/college-enrollment-input/college-enrollment-input.component';
-import { CollegeNameInputComponent } from './inputs/college-name-input/college-name-input.component';
-import { EmailInputComponent } from './inputs/email-input/email-input.component';
-import { FullNameInputComponent } from './inputs/full-name-input/full-name-input.component';
-import { PassInputComponent } from './inputs/pass-input/pass-input.component';
-import { CustomToastComponent } from './inputs/custom-toast/custom-toast.component';
-import { SpinnerLoadingComponent } from './inputs/spinner-loading/spinner-loading.component';
-import { ErroMessageComponent } from './inputs/erro-message/erro-message.component';
-import { CodeVerificationInputComponent } from './inputs/code-verification-input/code-verification-input.component';
-import { SideBarComponent } from './ui/side-bar/side-bar.component';
-import { HeaderLogInComponent } from './ui/header-log-in/header-log-in.component';
-import { RouterModule } from '@angular/router';
-import { ButtonReadingComponent } from './ui/button-reading/button-reading.component';
-import { ButtonWritingComponent } from './ui/button-writing/button-writing.component';
-import { ButtonSpeakingComponent } from './ui/button-speaking/button-speaking.component';
 import { ButtonListeningComponent } from './ui/button-listening/button-listening.component';
+import { ButtonReadingComponent } from './ui/button-reading/button-reading.component';
+import { ButtonSpeakingComponent } from './ui/button-speaking/button-speaking.component';
+import { ButtonWritingComponent } from './ui/button-writing/button-writing.component';
 import { CardActivityComponent } from './ui/card-activity/card-activity.component';
 import { CardActivityDirective } from './directives/card-activity.directive';
+import { CodeVerificationInputComponent } from './inputs/code-verification-input/code-verification-input.component';
+import { CollegeEnrollmentInputComponent } from './inputs/college-enrollment-input/college-enrollment-input.component';
+import { CollegeNameInputComponent } from './inputs/college-name-input/college-name-input.component';
+import { CustomToastComponent } from './inputs/custom-toast/custom-toast.component';
+import { EmailInputComponent } from './inputs/email-input/email-input.component';
+import { ErroMessageComponent } from './inputs/erro-message/erro-message.component';
+import { FullNameInputComponent } from './inputs/full-name-input/full-name-input.component';
 import { HeaderHomeComponent } from './ui/header-home/header-home.component';
+import { PassInputComponent } from './inputs/pass-input/pass-input.component';
+import { RouterModule } from '@angular/router';
+import { SideBarComponent } from './ui/side-bar/side-bar.component';
+import { SpinnerLoadingComponent } from './inputs/spinner-loading/spinner-loading.component';
 
 
 
@@ -48,7 +47,6 @@ import { HeaderHomeComponent } from './ui/header-home/header-home.component';
     CustomToastComponent,
     ErroMessageComponent,
     HeaderHomeComponent,
-    HeaderLogInComponent,
     SideBarComponent,
     SpinnerLoadingComponent,
   ],
@@ -79,7 +77,6 @@ import { HeaderHomeComponent } from './ui/header-home/header-home.component';
     CustomToastComponent,
     ErroMessageComponent,
     HeaderHomeComponent,
-    HeaderLogInComponent,
     SideBarComponent,
     SpinnerLoadingComponent,
   ]
