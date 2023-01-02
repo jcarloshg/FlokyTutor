5c655c2ee333bc2dd85b5a20534d9b867aed5adb
jcarloshg <carlosj12336@gmail.com>
2023-01-02 13:12:34 -0600

    delete input [app-editor-publications]

--- src/app/publications/publications.module.ts
+++ src/app/publications/publications.module.ts
@@ -3,12 +3,14 @@ import { CommonModule } from '@angular/common';
 
 import { PublicationsRoutingModule } from './publications-routing.module';
 import { HomePublicationsComponent } from './pages/home-publications/home-publications.component';
+import { SharedModule } from '../shared/shared.module';
+// view publications
 import { ViewPublicationsComponent } from './pages/view-publications/view-publications.component';
+import { ButtonCardToCreatePublicationComponent } from './pages/view-publications/components/button-card-to-create-publication/button-card-to-create-publication.component';
+// create publications
 import { CreatePublicationsComponent } from './pages/create-publications/create-publications.component';
-import { ButtonCardToCreatePublicationComponent } from './components/button-card-to-create-publication/button-card-to-create-publication.component';
 import { PublicationTitleInputComponent } from './pages/create-publications/components/publication-title-input/publication-title-input.component';
 import { PublicationBodyInputModule } from './pages/create-publications/components/publication-body-input/publication-body-input.module';
-import { SharedModule } from '../shared/shared.module';
 
 
 
