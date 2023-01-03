25de3b303f81cbddcfb8511750b0b89f4467ed3c
jcarloshg <carlosj12336@gmail.com>
2023-01-03 17:46:03 -0600

    fix view

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,7 +5,7 @@ import { ConfirmationModalService } from './shared/services/confirmation-modal.s
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  // styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'FlokyTutor';
