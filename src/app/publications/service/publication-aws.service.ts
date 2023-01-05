b891dc4614a8ed2cc4231e271d02d5f23107666b
jcarloshg <carlosj12336@gmail.com>
2023-01-04 18:43:28 -0600

    implement usecase [createPost]

--- src/app/publications/service/publication-aws.service.ts
+++ src/app/publications/service/publication-aws.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@angular/core';
+import { DataStore } from 'aws-amplify';
 import { InputCreatePost, ManagePost, PostResponse } from 'src/domain/useCases/managePost.useCase.interface';
 import { Loading } from '../../shared/services/loading';
+import { Post } from 'src/models';
 
 @Injectable({
   providedIn: 'root'
@@ -12,10 +14,20 @@ export class PublicationAWSService extends Loading implements ManagePost {
   }
 
   public async createPost(inputCreatePost: InputCreatePost): Promise<PostResponse> {
+
     this.isLoading = true;
-    await console.log(inputCreatePost);
+
+    const postCreated = await DataStore.save(
+      new Post({
+        title: inputCreatePost.title,
+        body: inputCreatePost.body,
+        tutorAccountID: inputCreatePost.tutorAccountID,
+      })
+    );
+
     this.isLoading = false;
-    return { isOk: true }
+    return { isOk: true, data: postCreated };
+
   }
 
 }
